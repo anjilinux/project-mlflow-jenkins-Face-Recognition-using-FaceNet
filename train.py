829a07439c608c8fad6fce9bf12118c8f0274b6c
anjilinux <anjireddy2024@gmail.com>
2026-02-03 15:12:00 +0530

    ddd

--- train.py
+++ train.py
@@ -3,7 +3,7 @@ import joblib
 from sklearn.svm import SVC
 from feature_engineering import generate_embeddings
 
-mlflow.set_tracking_uri("http://localhost:5000")
+mlflow.set_tracking_uri("http://localhost:5555")
 mlflow.set_experiment("FaceNet-Recognition")
 
 X, y = generate_embeddings("data/raw")
